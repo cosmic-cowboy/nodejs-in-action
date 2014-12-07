412c62b846d3ef6bbef6dfacfc9fa618080d80bf
hiroo takaku <slgerkamp@gmail.com>
2014-12-07 19:56:40 +0900

    chapter02 画面の表示：イベントハンドリングと描画

--- chapter_02/websocket_and_http_application/public/javascripts/chat_ui.js
+++ chapter_02/websocket_and_http_application/public/javascripts/chat_ui.js
@@ -1,3 +1,70 @@
+var socket = io.connect();
+
+$(document).ready(function () {
+	var chatApp = new Chat(socket);
+
+	// 各種イベントハンドリング
+
+	// 名前変更リクエスト結果
+	socket.on('nameResult', function (result) {
+		var message;
+
+		if(result.success){
+			message = 'You are now known as ' + result.name + '.';
+		} else {
+			message = result.message;
+		}
+
+		$('#messages').append(divSystemContentElement(message));
+	});
+
+	// ルーム変更リクエスト結果
+	socket.on('joinResult', function (result) {
+		$('#room').text(result.room);
+		$('#messages').append(divSystemContentElement('Room changed.'));
+	});
+
+	// 受信メッセージを表示
+	socket.on('message', function (message) {
+		var newElement = $('<div></div>').text(message.text);
+		$('#messages').append(newElement);
+	});
+
+	// 利用できるルームリスト表示
+	socket.on('rooms', function (rooms) {
+		$('#room-list').empty();
+		for(var room in rooms){
+			room = room.substring(1, room.length);
+			if(room !== ''){
+				$('#room-list').append(divEscapedContentElement(room));
+			}
+		}
+		// クリックしたルームに移動
+		$('#room-list div').click(function () {
+			chatApp.processCommand('/join ' + $(this).text());
+			$('#send-message').focus();
+		});
+	});
+
+	// 名前変更リクエスト結果
+	socket.on('nameResult', function (result) {
+	});
+
+	// 利用できるルームのリストは周期的に問い合わせる
+	setInterval(function(){
+		socket.emit('rooms');
+	}, 1000);
+
+	$('#send-message').focus();
+
+	// チャットメッセージ送信用フォーム
+	$('#send-form').submit(function () {
+		processUserInput(chatApp, socket);
+		return false;
+	});
+});
+
+
 //////////////////////
 // ヘルパー関数
 //////////////////////
