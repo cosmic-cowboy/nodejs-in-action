876e9a2978809194afdceb4a48d60cbf88e0d1e8
hiroo takaku <slgerkamp@gmail.com>
2014-12-14 17:36:45 +0900

    chapter03 非同期プログラミング：eventemitterで１回のイベントを処理

---
+++ chapter_03/eventemitter/tcpsocket_data_event/echo_server_at_once.js
@@ -0,0 +1,12 @@
+var net = require('net');
+
+var server = net.createServer(function (socket) {
+	// socketはeventemitter
+	// socketのonメソッドを利用してに、dataイベントのリスナを登録する
+	// dataイベントはソケットに新しいデータが到着するたびemit（発行される）
+	socket.once('data', function (data) {
+		socket.write(data);
+	});
+});
+
+server.listen(8888);
