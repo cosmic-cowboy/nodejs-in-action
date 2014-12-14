2934521504853a7306990d14f43aae5c9bb8b8dd
hiroo takaku <slgerkamp@gmail.com>
2014-12-14 17:43:42 +0900

    役割分担を明確にする

--- chapter_03/eventemitter/event_emitter_extend/event_emitter_extend.js
+++ chapter_03/eventemitter/event_emitter_extend/event_emitter_extend.js
@@ -31,13 +31,30 @@ Watcher.prototype.start = function start() {
 // ファイルを処理するメソッド
 Watcher.prototype.watch = function watch() {
 	var watcher = this;
+
+	// イベントリスナの登録
+	watcher.on('process', function process (file) {
+		var watchFile = watchDir +'/'+ file;
+		var processedFile = processedDir +'/'+ file;
+
+		fs.rename(watchFile, processedFile, function(err){
+			if(err) {
+				console.log(err);
+				throw err;
+			}
+		});
+		console.log("done file : " + file);
+	});
+
 	fs.readdir(watchDir, function (err, files) {
 		if(err) throw err;
 		for(var index in files){
 			console.log("watch file : " + files[index]);
+			// イベントの発行
 			watcher.emit('process', files[index]);
 		}
 	});
 };
+
 
 module.exports = exports = Watcher;
--- chapter_03/eventemitter/event_emitter_extend/execute.js
+++ chapter_03/eventemitter/event_emitter_extend/execute.js
@@ -1,16 +1,6 @@
-var fs  = require('fs');
+
 var Watcher = require('./event_emitter_extend');
 
 var watcher = new Watcher();
-
-watcher.on('process', function process (file) {
-	var watchFile = './watch/' + file;
-	var processedFile = './done/' + file;
-
-	fs.rename(watchFile, processedFile, function(err){
-		if(err) throw err;
-	});
-	console.log("done file : " + file);
-});
 
 watcher.start();
