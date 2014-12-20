82677667c01b94a6d5cf90c6849d69f345e89b4d
hiroo takaku <slgerkamp@gmail.com>
2014-12-20 17:46:59 +0900

    chapter04 HTTPSアプリケーションのセキュリティ確保

---
+++ chapter_04/https/https.js
@@ -0,0 +1,14 @@
+var https = require('https');
+var fs    = require('fs');
+
+var options = {
+	key  : fs.readFileSync('./key.pem'),
+	cert : fs.readFileSync('./key-cert.pem')
+};
+
+var server = https.createServer(options, function (req, res) {
+	res.wwriteHead(200);
+	res.end('Hello World\n');
+});
+
+server.listen(3000);
