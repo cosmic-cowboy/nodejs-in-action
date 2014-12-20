190f1e5eb4434b8eea560f12bcd0155a772146f3
hiroo takaku <slgerkamp@gmail.com>
2014-12-20 15:24:41 +0900

    chapter04 静的ファイルの供給：fs.statを利用したエラーハンドリング

--- chapter_04/static_assets/staticFileServer.js
+++ chapter_04/static_assets/staticFileServer.js
@@ -19,16 +19,36 @@ var server = http.createServer(function(req, res){
 	// ドメイン以下のpathを取得し、ファイルのルートと結合し、
 	// 絶対パスを作成
 	var path = join(root, url.pathname);
-	// fs.ReadStreamを作成
-	var stream = fs.createReadStream(path);
-	// 読み込みが終わったらres.endを呼び出し、レスポンスを終了させる
-	stream.pipe(res);
 
-	// データ読み込み中にエラーが発生した場合
-	stream.on('error', function (err) {
-		res.statusCode = 500;
-		res.end('Internal Server error');
+	// ファイルの存在チェック
+	fs.stat(path, function (err, stat) {
+		if(err){
+			// ファイルが存在しない
+			if('ENOENT' === err.code){
+				res.statusCode = 404;
+				res.end('Not Found');
+			} else {
+				res.statusCode = 500;
+				res.end('Internal Server error');
+			}
+
+		} else {
+			res.setHeader('Content-Length', stat.size);
+			// fs.ReadStreamを作成
+			var stream = fs.createReadStream(path);
+			// 読み込みが終わったらres.endを呼び出し、レスポンスを終了させる
+			stream.pipe(res);
+
+			// データ読み込み中にエラーが発生した場合
+			stream.on('error', function (err) {
+				res.statusCode = 500;
+				res.end('Internal Server error');
+			});
+
+		}
 	});
+
+
 });
 
 server.listen(3000);
