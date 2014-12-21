553d9b8a58c19c10f0ad2bd92e71e4905e6ba481
hiroo takaku <slgerkamp@gmail.com>
2014-12-21 15:47:36 +0900

    chapter05 リレーショナルデータベース管理システム：MySQLへのデータ操作

--- chapter_05/relational_database_management_system/lib/timetrack.js
+++ chapter_05/relational_database_management_system/lib/timetrack.js
@@ -10,7 +10,7 @@ exports.sendHtml = function sendHtml (res, html) {
 };
 
 // HTTPのPOSTデータ解析
-exports.parseReceivedData = function parseReceivedDataq (req, callback) {
+exports.parseReceivedData = function parseReceivedData (req, callback) {
 	var body = '';
 	req.setEncoding('utf8');
 	req.on('data', function (chunk) {
@@ -23,7 +23,7 @@ exports.parseReceivedData = function parseReceivedDataq (req, callback) {
 };
 
 // 単純なフォームのレンダリング
-exports.actionForm = function (id, path, label) {
+exports.actionForm = function actionForm (id, path, label) {
 	var html = '<form method="POST" action="' + path + '">' +
 			'<input type="hidden" name="id" value="' + id + '">' +
 			'<input type="submit" value="' + label + '">' +
@@ -31,3 +31,78 @@ exports.actionForm = function (id, path, label) {
 	return html;
 };
 
+// RDBMSへのCRUD
+// 作業日報の追加
+exports.add = function add (db, req, res) {
+	// POSTデータの解析
+	parseReceivedData(req, function (work) {
+		db.query(
+			"INSERT INTO work (hours, date, description) " +
+			" VALUES (?, ?, ?)",
+			[work.hours, work.date, work.description],
+			function (err) {
+				if(err) throw err;
+				// 作業日報リストを表示
+				show(db, res);
+			}
+		);
+	});
+};
+
+// 作業日報の削除
+exports.del = function del (db, req, res) {
+	// POSTデータの解析
+	parseReceivedData(req, function (work) {
+		db.query(
+			"DELETE FROM work WHERE id = ?",
+			[work.id],
+			function (err) {
+				if(err) throw err;
+				// 作業日報リストを表示
+				show(db, res);
+			}
+		);
+	});
+};
+
+// 作業日報のアーカイブ化
+exports.archive = function archive (db, req, res) {
+	// POSTデータの解析
+	parseReceivedData(req, function (work) {
+		db.query(
+			"UPDATE work SET archived=1 id = ?",
+			[work.id],
+			function (err) {
+				if(err) throw err;
+				// 作業日報リストを表示
+				show(db, res);
+			}
+		);
+	});
+};
+
+exports.show = function show (db, res, showArchived) {
+	var query = "SELECT * FROM work " +
+			" WHERE archived=? " +
+			" ORDER BY date DESC ";
+	var archiveValue = (showArchived) ? 1 : 0;
+	db.query(
+		query,
+		[archiveValue],
+		function (err, rows) {
+			if(err) throw err;
+			html = (showArchived) ? '' : '<a href="/archived">Archived Work}<br/>';
+			html += exports.workHitlistHtml(rows);
+			html += exports.workFromHtml();
+			exports.sendHtml(res, html);
+		}
+	);
+};
+
+exports.workHitlistHtml = function workHitlistHtml(){
+
+};
+
+exports.workFromHtml = function workFromHtml(){
+
+};
