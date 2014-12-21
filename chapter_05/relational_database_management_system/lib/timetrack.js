ee0f95dcba0fc3ef24dce449900f43a1c81d57bc
hiroo takaku <slgerkamp@gmail.com>
2014-12-21 15:14:08 +0900

    chapter05 リレーショナルデータベース管理システム：HTML送信、フォーム作成、フォームデータ受信を行うヘルパー関数

--- chapter_05/relational_database_management_system/lib/timetrack.js
+++ chapter_05/relational_database_management_system/lib/timetrack.js
@@ -1,1 +1,33 @@
-console.log('timetrack');
+var qs    = require('querystring');
+
+// HTML送信、フォーム作成、フォームデータ受信を行うヘルパー関数
+
+// HTML応答を送信
+exports.sendHtml = function sendHtml (res, html) {
+	res.setHeader('Content-Type', 'text/html');
+	res.setHeader('Content-Length', Buffer.byteLength(html));
+	res.end(html);
+};
+
+// HTTPのPOSTデータ解析
+exports.parseReceivedData = function parseReceivedDataq (req, callback) {
+	var body = '';
+	req.setEncoding('utf8');
+	req.on('data', function (chunk) {
+		body += chunk;
+	});
+	req.on('end', function () {
+		var data = qs.parse(body);
+		callback(data);
+	});
+};
+
+// 単純なフォームのレンダリング
+exports.actionForm = function (id, path, label) {
+	var html = '<form method="POST" action="' + path + '">' +
+			'<input type="hidden" name="id" value="' + id + '">' +
+			'<input type="submit" value="' + label + '">' +
+			'</form>';
+	return html;
+};
+
