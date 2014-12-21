3c7dc20cc82af571d240ba398a13c11d5635b3cf
hiroo takaku <slgerkamp@gmail.com>
2014-12-21 16:39:10 +0900

    chapter05 NoSQLデータベース：Redisでのデータ操作（ハッシュ表）

--- chapter_05/NoSQL_database/connect_redis.js
+++ chapter_05/NoSQL_database/connect_redis.js
@@ -14,3 +14,23 @@ client.get('color', function (err, value) {
 	if(err) throw err;
 	console.log("Got : " + value);
 });
+
+// ハッシュマップでの保存
+client.hmset('camping', {
+	'shelter' : '2-person tent',
+	'cooking' : 'campstove'
+}, redis.print);
+
+// キーを指定した取り出し
+client.hget('camping', 'cooking', function (err, value){
+	if(err) throw err;
+	console.log('will be cooking with : ' + value);
+});
+
+// ハッシュマップ内のデータを取り出し
+client.hkeys('camping', function (err, keys) {
+	if(err) throw err;
+	keys.forEach(function (key, i) {
+		console.log(i + ': ' + key);
+	});
+});
