19ca109d996436da41c7fa2b1fa880f4bcd9ca85
hiroo takaku <slgerkamp@gmail.com>
2014-12-21 16:29:23 +0900

    chapter05 NoSQLデータベース：Redisでのデータ操作（キーペア）

--- chapter_05/NoSQL_database/connect_redis.js
+++ chapter_05/NoSQL_database/connect_redis.js
@@ -1,7 +1,16 @@
 var redis = require('redis');
-var client = redis.createClient('6379','127.0.0.1');
+var client = redis.createClient();
 
 // エラーハンドリング
 client.on('error',function(err){
 	console.log("ERROR : " + err);
+});
+
+// キーペアでの保存
+client.set('color', 'red', redis.print);
+
+// キーペアでの取り出し
+client.get('color', function (err, value) {
+	if(err) throw err;
+	console.log("Got : " + value);
 });
