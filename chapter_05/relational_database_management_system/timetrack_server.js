54f9f34bab0d2e13e3ee2b883b795dd9dc055fd3
hiroo takaku <slgerkamp@gmail.com>
2014-12-21 14:56:37 +0900

    chapter05 リレーショナルデータベース管理システム：データベーステーブル作成

--- chapter_05/relational_database_management_system/timetrack_server.js
+++ chapter_05/relational_database_management_system/timetrack_server.js
@@ -2,15 +2,8 @@ var http  = require('http');
 var work  = require('./lib/timetrack');
 var mysql = require('mysql');
 
-// mysqlに接続
-var db = mysql.createConnection({
-	host : '127.0.0.1',
-	user : 'myuser',
-	password : 'mypassword',
-	database : 'timetrack'
-});
-
 // HTTP リクエストの仕分け（ルーティング）
+// ※ルーティング部分はモジュール化した方が良い
 var server = http.createServer(function (req, res) {
 	switch(req.method){
 		// HTTP POSTリクエストの処理手順
@@ -41,3 +34,33 @@ var server = http.createServer(function (req, res) {
 		break;
 	}
 });
+
+//////////////////////
+/////  mysql関連  /////
+//////////////////////
+// ※モジュール化した方が良い
+
+// mysqlに接続
+var db = mysql.createConnection({
+	host : '127.0.0.1',
+	user : 'myuser',
+	password : 'mypassword',
+	database : 'timetrack'
+});
+
+// データベーステーブル作成
+db.query(
+	"CREATE TABLE IF NOT EXISTS work (" +
+	" id INT(10) NOT NULL AUTO_INCREMENT, " +
+	" hours DECIMAL(5,2) DEFAULT 0, " +
+	" date DATE, " +
+	" archived INT(1) DEFAULT 0, " +
+	" description LONGTEXT, " +
+	" PRIMARY KEY(id)) ",
+	function (err) {
+		if(err) throw err;
+		console.log('Server started...');
+		// HTTPサーバー起動
+		server.listen(3000, '127.0.0.1');
+	}
+);
