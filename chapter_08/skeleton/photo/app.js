b4924154da91c8b008eab84c0737c53d0acfa09c
hiroo takaku <slgerkamp@gmail.com>
2014-12-30 17:58:42 +0900

    chapter08 Express：EJSテンプレートエンジンを使用するアプリケーション解説

--- chapter_08/skeleton/photo/app.js
+++ chapter_08/skeleton/photo/app.js
@@ -13,21 +13,28 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
+// viewファイルを/viewsから供給
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// ファビコン
 app.use(express.favicon());
+// ログ出力
 app.use(express.logger('dev'));
+// リクエストの解析
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
+// 静的ファイルを/publicから供給
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
+	// 配置後はHTMLエラーページをスタイルつきで表示
   app.use(express.errorHandler());
 }
 
+// アプリケーションの経路指定
 app.get('/', routes.index);
 app.get('/users', user.list);
 
