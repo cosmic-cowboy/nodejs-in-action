c41c8955a3e76c9bd858d5ba3f60b89f463e204e
hiroo takaku <slgerkamp@gmail.com>
2014-12-31 18:30:39 +0900

    chapter09 Advanced Express：ログイン・ログアウトの実装

--- chapter_09/shoutbox/routes/login.js
+++ chapter_09/shoutbox/routes/login.js
@@ -1,11 +1,34 @@
+var User = require('../lib/user');
+
 // ログインフォームを表示
 exports.form = function (req, res) {
 	res.render('login', {title : 'ログイン'});
 };
 
+// ログイン認証処理
 exports.submit = function (req, res) {
+	var data = req.body.user;
+	User.authenticate(data.name, data.pass, function (err, user) {
+		if(err) return fn(err);
+
+		if(user){
+			req.session.uid = user.id;
+			// エントリーリストのページにリダイレクト
+			res.redirect('/');
+		} else {
+			res.error("ユーザを確認できませんでした");
+			// ログインフォームにリダイレクト
+			res.redirect('back');
+		}
+	});
 };
+
+// ログアウト処理
 exports.logout = function (req, res) {
+	req.session.destroy(function (err) {
+		if(err) throw err;
+		res.redirect('/');
+	});
 };
 
 
