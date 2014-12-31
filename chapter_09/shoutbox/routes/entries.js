d770a8d5e698f2f73f3b36677307fe52cc237232
hiroo takaku <slgerkamp@gmail.com>
2014-12-31 19:45:20 +0900

    chapter09 Advanced Express：エントリーの作成の実装

--- chapter_09/shoutbox/routes/entries.js
+++ chapter_09/shoutbox/routes/entries.js
@@ -15,5 +15,16 @@ exports.form = function (req, res, next) {
 	res.render('post', {title:'記事投稿'});
 };
 exports.submit = function (req, res, next) {
-	// body...
+	var data = req.body.entry;
+
+	var entry = new Entry({
+		username : res.locals.user.name,
+		title : data.title,
+		body : data.body
+	});
+
+	entry.save(function (err) {
+		if(err) return next(err);
+		res.redirect('/');
+	});
 };
