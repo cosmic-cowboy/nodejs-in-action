b02fda7949da661e394f55dae87aabc54fad455f
hiroo takaku <slgerkamp@gmail.com>
2014-12-31 13:06:52 +0900

    chapter09 Advanced Express：ユーザ情報をRedisに保存

--- chapter_09/shoutbox/lib/user.js
+++ chapter_09/shoutbox/lib/user.js
@@ -9,3 +9,32 @@ function User (obj) {
 		this[key] = obj[key];
 	}
 }
+
+User.prototype.save = function(fn) {
+	if(this.id){
+		this.update(fn);
+	} else {
+		var user = this;
+		db.incr('user:ids', function (err, id) {
+			if(err) return fn(err);
+			user.id = id;
+			user.hashPassword(function (err) {
+				if(err) return fn(err);
+				user.update(fn);
+			});
+		});
+	}
+};
+
+User.prototype.update = function(fn) {
+	var user = this;
+	var id = user.id;
+	// 名前でユーザIDをインデックス参照
+	db.set('user:id:' + user.name, id, function (err) {
+		fn(err);
+		// Redisのハッシュにデータを保存
+		db.hmset('user:' + id, user, function (err) {
+			fn(err);
+		});
+	});
+};
