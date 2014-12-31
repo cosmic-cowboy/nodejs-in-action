80306cee6427ad2698c3a7962d6b684712bc57bc
hiroo takaku <slgerkamp@gmail.com>
2014-12-31 18:05:49 +0900

    chapter09 Advanced Express：ユーザ登録処理

--- chapter_09/shoutbox/routes/register.js
+++ chapter_09/shoutbox/routes/register.js
@@ -1,7 +1,28 @@
+var User = require('../lib/user');
+
 exports.form = function (req, res) {
 	res.render('register', {title : '登録ください'});
 };
 
-exports.submit = function () {
-	// body...
+exports.submit = function (req, res, next) {
+	var data = req.body.user;
+	User.getByName(data.name, function (err, user) {
+		// データベース接続エラー
+		if(err) return next(err);
+
+		if(user.id){
+			res.error("この名前はすでに登録されています");
+			res.redirect('back');
+		} else {
+			user = new User({
+				name : data.name,
+				pass : data.pass
+			});
+			user.save(function (err) {
+				if(err) return next(err);
+				req.session.uid = user.id;
+				res.redirect('/');
+			});
+		}
+	});
 };
