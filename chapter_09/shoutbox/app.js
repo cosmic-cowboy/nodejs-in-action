c1f9568cf93711272a9f7c5b38e21ef112d4e931
hiroo takaku <slgerkamp@gmail.com>
2014-12-31 16:09:01 +0900

    chapter09 Advanced Express：登録画面作成

--- chapter_09/shoutbox/app.js
+++ chapter_09/shoutbox/app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var register = require('./routes/register');
 var http = require('http');
 var path = require('path');
 
@@ -31,6 +32,10 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+// ユーザー登録の経路を追加
+app.get('/register', register.form);
+app.post('/register', register.submit);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ chapter_09/shoutbox/routes/register.js
@@ -0,0 +1,7 @@
+exports.form = function (req, res) {
+	res.render('register', {title : '登録ください'});
+};
+
+exports.submit = function () {
+	// body...
+};
