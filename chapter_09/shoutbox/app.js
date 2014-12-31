553e2979f80217d88b07f5beac88d241e6b865ea
hiroo takaku <slgerkamp@gmail.com>
2014-12-31 19:58:54 +0900

    経路ごとのミドルウェアでフォーム認証

--- chapter_09/shoutbox/app.js
+++ chapter_09/shoutbox/app.js
@@ -47,8 +47,35 @@ app.get('/logout', login.logout);
 // エントリーの関する経路の追加
 app.get('/', entries.list);
 app.get('/post', entries.form);
-app.post('/post', entries.submit);
+app.post('/post',
+	requireEntryTitle,
+	requireEntryTitleLengthAbove(4),
+	entries.submit);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+function requireEntryTitle (req, res, next) {
+	var title = req.body.entry.title;
+	if(title){
+		next();
+	} else {
+		res.error("タイトルを入力してください");
+		res.redirect('back');
+		return;
+	}
+}
+
+function requireEntryTitleLengthAbove(len){
+	return function (req, res, next) {
+		var title = req.body.entry.title;
+		if(title.length > 4){
+			next();
+		} else {
+			res.error("タイトルは四文字以上で入力してください");
+			res.redirect('back');
+			return;
+		}
+	};
+}
