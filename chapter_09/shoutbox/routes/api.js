cc12d864c16aa198df394ff253e102a2503ffbdb
hiroo takaku <slgerkamp@gmail.com>
2014-12-31 21:24:05 +0900

    ルーティングの実装

--- chapter_09/shoutbox/routes/api.js
+++ chapter_09/shoutbox/routes/api.js
@@ -3,14 +3,18 @@ var User = require('../lib/user');
 
 exports.auth = express.basicAuth(User.authenticate);
 
-exports.user = function () {
+exports.user = function (req, res, next) {
+	User.get(req.param('id'), function (err, user) {
+		if(err) return next(err);
+		if(!user.id) return res.send(400);
+		res.json(user);
+	});
+};
+
+exports.entries = function (req, res, next) {
 	
 };
 
-exports.entries = function () {
-	
-};
-
-exports.add = function () {
+exports.add = function (req, res, next) {
 	
 };
